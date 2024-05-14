7a177b3106be6dbac4d56cecbdb9341d7850668d
Martin Kovachev <miracle@nimasystems.com>
2024-05-14 16:10:05 +0300

    fix: https://trello.com/c/3khOUPNa/258-google-drive-import-addons

--- Helper/Drive.php
+++ Helper/Drive.php
@@ -15,10 +15,14 @@ use Google_Service_Drive;
 use Google_Service_Drive_DriveFile;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
+use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
 
 class Drive extends AbstractHelper
 {
+    // TODO: it seems this is always limited by Google to a maximum of 100
+    public const DEFAULT_PAGE_SIZE = 1000;
+
     protected Data $dataHelper;
     private ?Client $client = null;
 
@@ -93,13 +97,14 @@ class Drive extends AbstractHelper
         return $folders[$folder['parent'][0]]['name'];
     }
 
-    public function getFolders(Client $client = null, $nextPageToken = null): array
+    public function getFolders(Client $client = null, $nextPageToken = null, ?OutputInterface $output = null): array
     {
         $client = $client ?: $this->getClientInstance();
         $service = new Google_Service_Drive($client);
 
         $parameters['q'] = "mimeType='application/vnd.google-apps.folder' and trashed=false";
         $parameters['orderBy'] = 'name';
+        $parameters['pageSize'] = self::DEFAULT_PAGE_SIZE;
         $parameters['fields'] = 'files(id,name,parents,shared), nextPageToken';
 
         if (!empty($nextPageToken)) {
@@ -133,26 +138,37 @@ class Drive extends AbstractHelper
         return array_merge($folderNames, $this->getFolders($client, $nextPageToken));
     }
 
-    public function getFolderContents(string $folderId, bool $recursive = false, Google_Service_Drive $driveService = null): array
+    public function getFolderContents(string               $folderId, bool $recursive = false,
+                                      Google_Service_Drive $driveService = null,
+                                      callable             $pageResultsCallback = null,
+                                      ?OutputInterface     $output = null): array
     {
         $driveService = new Google_Service_Drive($this->getClientInstance());
         $optParams = [
             'fields' => 'nextPageToken, files(*)',
             'q' => "'$folderId' in parents and trashed=false",
+            'pageSize' => self::DEFAULT_PAGE_SIZE,
         ];
         $filesResult = [];
         $results = $driveService->files->listFiles($optParams);
         $nextPageToken = $results->getNextPageToken();
 
-        if (count($results->getFiles()) !== 0) {
-            $filesResult = $this->_getFolderContents($driveService, $folderId, $results->getFiles(), $recursive, $filesResult, $nextPageToken);
-        }
-
-        return $filesResult;
+        return $this->_getFolderContents($driveService, $folderId,
+            $results->getFiles(), $recursive, $filesResult, $pageResultsCallback, $nextPageToken, $output);
     }
 
-    private function _getFolderContents(Google_Service_Drive $driveService, $folderId, $files, bool $recursive, array &$filesResult, ?string $nextPageToken = null): array
+    private array $cachedDirs = [];
+
+    private function _getFolderContents(Google_Service_Drive $driveService, $folderId,
+                                                             $files, bool $recursive, array &$filesResult,
+                                        callable             $pageResultsCallback = null,
+                                        ?string              $nextPageToken = null,
+                                        ?OutputInterface     $output = null): array
     {
+//        $output->writeln('Getting folder contents for ' . $folderId . ', pageToken: ' . $nextPageToken);
+
+        $localFres = [];
+
         /** @var DriveFile $file */
         foreach ($files as $file) {
             $fileId = $file->getId();
@@ -160,23 +176,38 @@ class Drive extends AbstractHelper
             $fileSize = $file->getSize();
             $fileType = $file->getMimeType();
             $fileParents = $file->getParents();
+
+            $isFolder = $fileType == 'application/vnd.google-apps.folder';
+
+            if ($isFolder) {
+                // TODO: bug - folder name is still not available this way
+                $this->cachedDirs[$folderId] = $fileName;
+            }
+
             $return_file = [
                 'id' => $fileId,
                 'size' => $fileSize,
                 'name' => $fileName,
+                'dirName' => $this->cachedDirs[$folderId] ?? '',
                 'type' => $fileType,
                 'parents' => $fileParents,
             ];
-            $isFolder = $fileType == 'application/vnd.google-apps.folder';
+//            if (!$isFolder) {
+//                continue;
+//            }
+//            $output->writeln('Processing subfolder ' . $folderId . ' / ' .  $file->getName() . ', pageToken: ' . $nextPageToken);
+//            $recursive=true;
 
             if (!$isFolder) {
                 $filesResult[$fileId] = $return_file;
+                $localFres[$fileId] = $return_file;
             }
 
             if ($recursive && $isFolder) {
                 $optParams = [
                     'q' => "'$fileId' in parents and trashed=false",
                     'fields' => 'nextPageToken, files(*)',
+                    'pageSize' => self::DEFAULT_PAGE_SIZE,
                 ];
 
                 $results = $driveService->files->listFiles($optParams);
@@ -184,13 +215,23 @@ class Drive extends AbstractHelper
 
                 if (count($results->getFiles()) !== 0) {
                     $files_sub = $results->getFiles();
-                    $files_sub_arr = $this->_getFolderContents($driveService, $fileId, $files_sub, $recursive, $filesResult);
+                    $files_sub_arr = $this->_getFolderContents($driveService, $fileId, $files_sub, $recursive, $filesResult,
+                        $pageResultsCallback,
+                        null, $output);
 
                     foreach ($files_sub_arr as $file_sub) {
-                        $file_sub_id = $file_sub['file_id'];
+                        $file_sub_id = $file_sub['id'];
                         $filesResult[$file_sub_id] = $file_sub;
                     }
                 }
+            }
+        }
+
+        if ($pageResultsCallback) {
+            $ret = $pageResultsCallback($localFres);
+
+            if (!$ret) {
+                return $filesResult;
             }
         }
 
@@ -204,9 +245,11 @@ class Drive extends AbstractHelper
 
             $results = $driveService->files->listFiles($optParams);
             $nextPageToken = $results->getNextPageToken();
-            $filesResult = $this->_getFolderContents($driveService, $folderId, $results, $recursive, $filesResult, $nextPageToken);
+            $filesResult = $this->_getFolderContents($driveService, $folderId, $results, $recursive, $filesResult,
+                $pageResultsCallback,
+                $nextPageToken,
+                $output);
         }
-
 
         return $filesResult;
     }
